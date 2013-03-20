2f139e63173326b508bd3bf5582705b7286be0bb
ahuiwanglei <ahuiwanglei@126.com>
2013-03-20 13:25:07 +0800

    as

---
+++ src/com/aurora/test/git/App.java
@@ -0,0 +1,16 @@
+package com.aurora.test.git;
+/**
+ * @author 作者 E-mail:ahuiwanglei@126.com
+ * @version 创建时间：2013-3-19 下午2:25:19
+ * 类说明
+ */
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("ssssss");
+    }
+
+}
---
+++ src/com/aurora/test/git/Pig.java
@@ -0,0 +1,12 @@
+package com.aurora.test.git;
+/**
+ * @author 作者 E-mail:ahuiwanglei@126.com
+ * @version 创建时间：2013-3-20 上午10:20:21
+ * 类说明
+ */
+public class Pig {
+
+     private String name;
+     private String sss;
+
+}
